bff950de139e7970a3ab0af3658b35cdb5506055
5segments <222600307+5seg@users.noreply.github.com>
2026-02-08 22:38:40 +0900

    design changed to improve

--- src/routes/articles/[id]/+page.svelte
+++ src/routes/articles/[id]/+page.svelte
@@ -5,15 +5,15 @@
 	console.log(new Date(data.publishedAt).getTime());
 </script>
 
-<div class="mx-4 my-6 space-y-8 md:mx-auto md:w-lg">
+<div class="mx-4 my-6 space-y-8 md:mx-auto md:w-2xl">
 	<Card>
 		<h1>5seg's blog</h1>
 		<div class="px-6 text-start">
-			<div>
-				<h2>{data.title}</h2>
-				<p>{parseDate(data.publishedAt)}</p>
+			<div class="mb-2">
+				<h1>{data.title}</h1>
+				<p class="font-mono text-gray-500">{parseDate(data.publishedAt)}</p>
 			</div>
-			<div>
+			<div class="">
 				{@html data.content}
 			</div>
 		</div>
