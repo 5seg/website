3364f8b8a05b12d3a77d48520423be62164e3dbb
5segments <222600307+5seg@users.noreply.github.com>
2026-02-09 17:25:16 +0900

    change theme color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 <svelte:head>
 	<title>5segments</title>
 	<meta name="description" content="空に浮かぶ月を、君と見られる日が来るまで。" />
-	<meta name="theme-color" content="#381d24" />
+	<meta name="theme-color" content="#5d0e17" />
 	<link rel="icon" href={favicon} />
 </svelte:head>
 {@render children()}
