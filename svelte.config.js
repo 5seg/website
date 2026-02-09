6b3caeb92f7c5e115bc2d7d9cab4b2641e29cb33
5segments <222600307+5seg@users.noreply.github.com>
2026-02-09 16:33:53 +0900

    use deno adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@deno/svelte-adapter';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = { kit: { adapter: adapter() } };
